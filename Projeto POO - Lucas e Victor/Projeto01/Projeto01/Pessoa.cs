bb76b6432746de4d691e7f8ce3154d766cdd1bd1
Lucas Barreto Barbosa <101645521+LucasBarbosa22@users.noreply.github.com>
2022-08-02 11:31:43 -0300

    Add files via upload

---
+++ Projeto POO - Lucas e Victor/Projeto01/Projeto01/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01
+{
+    internal class Pessoa
+    {
+        private string nome;
+
+        public Pessoa()
+        {
+            this.nome = null;
+        }
+        
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+
+        }
+        public string Nome
+        { 
+            get { return nome; }
+            set { nome = value; } 
+        }
+
+
+        public override string ToString()
+        {
+            return (String.Format("Nome: {0}", nome));
+        }
+    }
+  
+}
---
+++ Projeto POO - Lucas e Victor/Projeto01/Projeto01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome da pessoa: "); string n = Console.ReadLine();
+            Console.Write("Digite a idade de {0}: ", n); int i  = int.Parse(Console.ReadLine());
+
+            p.Nome = "Thiagão da Massa";
+
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
